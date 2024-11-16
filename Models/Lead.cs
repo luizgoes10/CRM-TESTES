5de19beda7b615ddc25d55d46eec371009d2d6de
Luiz Carlos Goes <lcg.goes@hotmail.com>
2024-11-16 19:14:42 -0300

    ajustes enganjamento

--- Models/Lead.cs
+++ Models/Lead.cs
@@ -9,4 +9,20 @@
         public DateTime dt_created { get; set; }
         public int int_status_id { get; set; }
     }
+    public class Visit
+    {
+        public int int_id { get; set; }
+        public string str_user_id { get; set; }
+        public string str_page { get; set; }
+        public DateTime dt_visit_time { get; set; }
+    }
+
+    public class Campaign
+    {
+        public int int_id { get; set; }
+        public string str_name { get; set; }
+        public string str_description { get; set; }
+        public List<string> target_pages { get; set; }
+        public string str_messsage_templates { get; set; }
+    }
 }
